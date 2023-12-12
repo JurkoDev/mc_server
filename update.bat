4fc52da2079dd10c36b74503f83065150b8a4692
jurko <50539591+hribik100@users.noreply.github.com>
2023-12-12 21:30:08 +0100

    update script

---
+++ update.bat
@@ -0,0 +1,11 @@
+@echo off
+IF EXIST .git (
+    echo setup uz bol
+) ELSE (
+    echo setupujem
+    echo PRIDAJ SI PSYCHADELIC CHEMISTRY MANUALNE
+    git init
+    git remote add origin "https://github.com/JurkoDev/mc_server.git"
+)
+echo updatujem
+git pull origin master
